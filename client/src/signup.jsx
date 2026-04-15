6b4e39325b11ac36d7c422a5ef09ccca5c4c9a9a
Sadia Hakim Mollik <sadiamollik769@gmail.com>
2026-04-15 23:47:02 +0600

    Update signup.jsx

--- client/src/signup.jsx
+++ client/src/signup.jsx
@@ -23,7 +23,7 @@ function Signup() {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(formData);
-        axios.post('http://localhost:3001/register', formData)
+        axios.post('https://smilespark-a-smart-dental-app-backend.onrender.com/register', formData)
         .then(result => console.log(result))
         navigate('/login')
         .catch(error => console.error(error));
